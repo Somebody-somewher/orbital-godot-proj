18d8c2179658711a9daa9732f3f781da9058faa5
Tan I-hur <ihur.tan@gmail.com>
2025-05-31 21:32:23 +0800

    Milestone1 Exported Version

--- scripts/Events/TimedEffect/timed_effect.gd
+++ scripts/Events/TimedEffect/timed_effect.gd
@@ -4,11 +4,11 @@ class_name TimedEffect
 var effect_when_time : BoardEvent
 var round_count : int = 0
 
-func trigger(id_name : String, board_matrix, tile_pos : Vector2i) -> void:
+func trigger(board_matrix, tile_pos : Vector2i) -> void:
 	if round_count > 0:
 		round_count -= 1
 	elif round_count == 0:
-		effect_when_time.trigger(id_name, board_matrix, tile_pos)
+		effect_when_time.trigger(board_matrix, tile_pos)
 
 func set_counter(n : int) :
 	round_count = n
