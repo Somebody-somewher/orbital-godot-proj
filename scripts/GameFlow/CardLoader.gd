2289aabeaebdf8940904ccff2feaba886c66fae9
Somebody-somewher <73779218+Somebody-somewher@users.noreply.github.com>
2025-06-03 17:21:14 +0800

    quick fix to resourcegrp

--- scripts/GameFlow/CardLoader.gd
+++ scripts/GameFlow/CardLoader.gd
@@ -3,12 +3,15 @@ class_name CardLoader
 # https://github.com/derkork/godot-resource-groups
 
 @export var building_grp : ResourceGroup
+@export var event_grp : ResourceGroup
 
 
 static var buildings_dict = {}
 
+
 static var cardset_types : Array[CardSet] = []
 static var buildings : Array[BuildingData] = []
+static var events : Array[Event] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,7 +22,7 @@ func _ready() -> void:
 			buildings_dict.get_or_add(b.id_name, b)
 
 static func get_building_data(id : String) -> BuildingData:
-	print(buildings_dict)
+	#print(buildings_dict)
 	return buildings_dict.get(id)
 
 static func get_display_name(id : String) -> String:
