4e0e9f47626cf8f53002d69b8c2ef0b8c5f206e6
Tan I-hur <ihur.tan@gmail.com>
2025-06-10 01:47:14 +0800

    Card Compendium Completed

--- scripts/Compendium/compendium_menu.gd
+++ scripts/Compendium/compendium_menu.gd
@@ -10,9 +10,11 @@ func open_book(search_id : String = "") -> void:
 	if search_id != "":
 		# id will always be valid (called by middle clicking cards)
 		Signalbus.show_card_information.emit(search_id)
-	$Bookmark/Area2D/CollisionShape2D.disabled = true
 	$AnimationPlayer.play("enter_scene")
 
 func close_book() -> void:
 	$AnimationPlayer.play("exit_scene")
-	$Bookmark/Area2D/CollisionShape2D.disabled = false
+
+
+func _on_texture_button_pressed() -> void:
+	Signalbus.open_compendium.emit("")
