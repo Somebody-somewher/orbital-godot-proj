23aa7a86c57f0949ccad46559efba8fc9c5e09e7
Tan I-hur <ihur.tan@gmail.com>
2025-06-19 16:53:00 +0800

    Menus logic working and options working

--- scripts/Main Menu/options_menu.gd
+++ scripts/Main Menu/options_menu.gd
@@ -12,3 +12,33 @@ func animate(entering : bool) -> void:
 		get_node("AnimationPlayer").play("enter")
 	else:
 		get_node("AnimationPlayer").play("exit")
+		
+######### SOUND OPTIONS CODE ##################
+
+@onready var master_label: Label = $SettingsTabs/LeftTab/TabContainer/Sound/MarginContainer/VBoxContainer/MasterBox/Label2
+@onready var music_label: Label = $SettingsTabs/LeftTab/TabContainer/Sound/MarginContainer/VBoxContainer/MusicBox/Label2
+@onready var sfx_label: Label = $SettingsTabs/LeftTab/TabContainer/Sound/MarginContainer/VBoxContainer/SFXBox/Label2
+
+func _on_music_slider_value_changed(value: float) -> void:
+	music_label.text = str(int(value))
+	AudioManager.change_bgm_volume(value/100)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	sfx_label.text = str(int(value))
+	AudioManager.sfx_volume = value/100
+
+func _on_sfx_slider_drag_ended(value_changed: bool) -> void:
+	AudioManager.play_sfx("grass")
+
+func _on_master_slider_value_changed(value: float) -> void:
+	master_label.text = str(int(value))
+	AudioManager.change_master_volume(value/100)
+	
+
+########## GRAPHICS OPTIONS #######################
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
