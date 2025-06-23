cd550a121a70d21e659d7ab41f888d8d8f33a460
Tan I-hur <ihur.tan@gmail.com>
2025-06-23 18:30:56 +0800

    Multiplayer menu and extra sabos and powers

---
+++ scripts/Main Menu/multiplayer_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+class_name MultiplayerMenu
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func animate(entering : bool) -> void:
+	if entering:
+		get_node("AnimationPlayer").play("enter")
+	else:
+		get_node("AnimationPlayer").play("exit")
