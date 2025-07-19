d06f3c90d485d44b558b10d186d60567543cdc81
Somebody-somewher <73779218+Somebody-somewher@users.noreply.github.com>
2025-07-20 00:58:54 +0800

    small update

--- scripts/Env & Buildings/Building.gd
+++ scripts/Env & Buildings/Building.gd
@@ -3,6 +3,7 @@ class_name Building
 
 # allows for scoring can move once scoring system is finalized
 static var building_scene: PackedScene = load("res://scenes/Building.tscn")
+#static var foil_mat : Material = load("res://shader/Foil/foil_mat.tres")
 
 # factory constructor
 static func new_building(building_name : String) -> Building:
@@ -16,8 +17,8 @@ static func new_building_frm_data(data_inst : BuildingInstanceData) -> Building:
 	var ret_building : Building = building_scene.instantiate()
 	ret_building.data_instance = data_inst
 	ret_building.get_node("EntityImage").texture = data_inst.get_data().card_sprite
-	#ret_building.data_instance.place_effects.append(data_inst.get_data().preview_event)
-	#ret_building.data_instance.get_data().place_effects.append(data_inst.get_data().preview_event)
+	#if data_inst.foil:
+		#ret_building.material = foil_mat
 	ret_building.z_index = 1
 	ret_building.name = data_inst.get_id()
 	return ret_building
