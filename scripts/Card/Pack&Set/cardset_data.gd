999c7ac89c58ab2188e84fb78f45cdf678c71a1f
Tan I-hur <ihur.tan@gmail.com>
2025-07-21 16:51:57 +0800

    Set tags and is_singleplayer flag set correctly

--- scripts/Card/Pack&Set/cardset_data.gd
+++ scripts/Card/Pack&Set/cardset_data.gd
@@ -6,12 +6,20 @@ class_name CardSetData
 
 # Technically could be CardData instead of Strings
 @export var cards : Dictionary[String, int]
-@export var is_multiplayer : bool 
 
+@export_category("Tags and Combos")
+enum CATEGORY { 
+	Sabotage, Economy, Religion, 
+	Infrastructure, Farming, Tech, 
+	Culture, Nature, Industry, Power }
+
+# influences some auras and card back colour
+@export var category: CATEGORY
 # range from 0 - 3 (common, uncommon, rare, epic)
 @export var rarity : int = 0
 @export var aquatic : bool
-
 @export var sabotage : bool
+@export var is_singleplayer : bool 
+
 func get_id() -> String:
 	return set_name
