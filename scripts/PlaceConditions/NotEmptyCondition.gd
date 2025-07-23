708c37fe38c7f9afb8690a0545c4c0dd7ed9b2df
Tan I-hur <ihur.tan@gmail.com>
2025-07-23 18:31:11 +0800

    All PlaceConditions finalsed

--- scripts/PlaceConditions/NotEmptyCondition.gd
+++ scripts/PlaceConditions/NotEmptyCondition.gd
@@ -3,7 +3,5 @@ class_name NotEmptyCondition
 
 #for some power cards
 func test(board : BoardMatrixData, tile_pos : Vector2i, source : CardInstanceData) -> bool:
-	var placeable_arr = board.board_matrix.get_tile(tile_pos).placeable_arr
-	if placeable_arr.is_empty():
-		return false
-	return true
+	var placeable_arr = board.get_tile(tile_pos).placeable_arr
+	return !placeable_arr.is_empty()
--- scripts/PlaceConditions/StackOnSomeCondition.gd
+++ scripts/PlaceConditions/StackOnSomeCondition.gd
@@ -1,12 +1,14 @@
-extends BoardCondition
-class_name StackOnSomeCondition
+extends StackableCondition
+class_name StackOnlyOnCondition
 
 @export var stackable_buildings : Array[String]
 
+
+#can stack only on listed buildings
 func test(board : BoardMatrixData, tile_pos : Vector2i, source : CardInstanceData) -> bool:
-	var placeable_arr = board.board_matrix.get_tile(tile_pos).placeable_arr
+	var placeable_arr = board.get_tile(tile_pos).placeable_arr
 		
 	for pn in placeable_arr:
 		if pn.data.id_name in stackable_buildings:
-			return false
-	return true
+			return true
+	return false
