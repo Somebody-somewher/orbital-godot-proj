bac2a3b50d3a99a33b7e2490f435f53fbb7f9c69
Tan I-hur <ihur.tan@gmail.com>
2025-07-24 14:53:44 +0800

    Cherry pick

--- scripts/Compendium/compendium_left_page.gd
+++ scripts/Compendium/compendium_left_page.gd
@@ -6,7 +6,7 @@ var matches : Array[CompendiumCard] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for b_data in CardLoader.buildings:
-		if !b_data.hidden:
+		if b_data and !b_data.hidden:
 			var new_child = CompendiumCard.new_button(b_data.id_name)
 			$ScrollContainer/GridContainer.add_child(new_child)
 			items.append(new_child)
