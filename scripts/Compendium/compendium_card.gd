a28b2163cbfe8c3c512bfbe6275f729226e63a53
Tan I-hur <ihur.tan@gmail.com>
2025-07-27 02:04:12 +0800

    Compendium Can search by tags

--- scripts/Compendium/compendium_card.gd
+++ scripts/Compendium/compendium_card.gd
@@ -3,6 +3,7 @@ class_name CompendiumCard
 
 var id_name : String = "obelisk"
 var search_name : String
+var tags : Array[String]
 
 static var button_scene : PackedScene = preload("res://scenes/Compendium/compendium_card.tscn")
 
@@ -15,10 +16,11 @@ func _ready() -> void:
 			$CardBackButton.texture_normal = load("res://assets/card_sprites/power_card.png")
 		_:
 			$CardBackButton.texture_normal = load("res://assets/card_sprites/blank_card.png")
-	 
+	
 	$CardImage.texture = data.card_sprite
 	search_name =  data.display_name
 	$Label.text = data.display_name
+	tags = data.tags
 
 static func new_button(id : String) -> CompendiumCard:
 	var button = button_scene.instantiate()
@@ -28,3 +30,9 @@ static func new_button(id : String) -> CompendiumCard:
 func _on_button_pressed() -> void:
 	AudioManager.play_sfx("click")
 	Signalbus.show_card_information.emit(id_name)
+
+func in_tags(str : String) -> bool:
+	for tag in tags:
+		if str in tag:
+			return true
+	return false
